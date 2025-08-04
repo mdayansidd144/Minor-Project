b522b87a263cb054c991f0b09addbd47b7ceba31
mdayansidd144 <ayansiddiqui1411@gmail.com>
2025-08-04 11:55:02 +0530

    Add files via upload
    here is an overview of this project

---
+++ stt_simple..py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+#this package is for the speech recognition and other packages are as follows
+recognizer = sr.Recognizer()
+with sr.Microphone() as source :
+    print("hello Say something...")
+    audio = recognizer.listen(source)
+    try:
+        print("you said:"+ recognizer.recognize_google(audio))
+    except sr.unknownValueError:
+        print("Sorry, i could not understand the audio.")
+    except sr.RequestError:
+        print("Could not connect to the service.")
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("you said:"+recognizer.recognize_google(audio))
+engine.runAndWait()
+import tkinter as tk
+from tkinter import scrolledtext
+window= tk.Tk()
+window.geometry("400*200")
+text_area =  ScrolledText.ScrolledText(window,width=50,height = 10)
+text_area.pack()
+window.mainloop()
+# the packages are use to decide that several task has to be perform
+# this isalla about the new speech to text converter and the valueis callled
